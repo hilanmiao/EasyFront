8b57af2744eb82db39b3fa68502cdcf4b0395e5a
Lan Miao <hilanmiao@126.com>
2017-12-22 14:59:08 +0800

    Add files via upload
    改造jquery-weui年月日时分秒5级选择控件

---
+++ datetime-picker/js/datetime-picker.js
@@ -0,0 +1,283 @@
+/* global $:true */
+/* jshint unused:false*/
+
++function ($) {
+    "use strict";
+
+    var defaults;
+
+    var formatNumber = function (n) {
+        return n < 10 ? "0" + n : n;
+    }
+
+    var Datetime = function (input, params) {
+        this.input = $(input);
+        this.params = params || {};
+
+        this.initMonthes = params.monthes
+
+        this.initYears = params.years
+
+        var p = $.extend({}, params, this.getConfig());
+        $(this.input).picker(p);
+    }
+
+    // 自定义当前天
+    var currentDay = 0
+
+    Datetime.prototype = {
+        getDays: function (max) {
+            var days = [];
+            for (var i = 1; i <= (max || 31); i++) {
+                days.push(i < 10 ? "0" + i : i);
+            }
+            return days;
+        },
+
+        getDaysByMonthAndYear: function (month, year) {
+            var int_d = new Date(year, parseInt(month) + 1 - 1, 1);
+            var d = new Date(int_d - 1);
+            return this.getDays(d.getDate());
+        },
+        getConfig: function () {
+            var today = new Date(),
+                params = this.params,
+                self = this,
+                lastValidValues;
+
+            var config = {
+                rotateEffect: false,  //为了性能
+                cssClass: 'datetime-picker',
+
+                value: [today.getFullYear(), formatNumber(today.getMonth() + 1), formatNumber(today.getDate()), formatNumber(today.getHours()), (formatNumber(today.getMinutes()))],
+
+                onChange: function (picker, values, displayValues) {
+                    var cols = picker.cols;
+                    var days = self.getDaysByMonthAndYear(values[1], values[0]);
+                    var currentValue = values[2];
+                    if (currentValue > days.length) {
+                        currentValue = days.length;
+                    }
+
+                    if (values[1] != currentDay) {
+                        currentDay = values[1]
+                        var daysNew = days.map(function (value) {
+                            var dayNew = new Date(values[0], parseInt(values[1]) - 1, value).getDay()
+                            switch (dayNew) {
+                                case 0:
+                                    dayNew = '日'
+                                    break;
+                                case 1:
+                                    dayNew = '一'
+                                    break;
+                                case 2:
+                                    dayNew = '二'
+                                    break;
+                                case 3:
+                                    dayNew = '三'
+                                    break;
+                                case 4:
+                                    dayNew = '四'
+                                    break;
+                                case 5:
+                                    dayNew = '五'
+                                    break;
+                                case 6:
+                                    dayNew = '六'
+                                    break;
+                            }
+                            dayNew = '星期' + dayNew;
+                            return value + dayNew;
+                        });
+                        picker.cols[4].replaceValues(days, daysNew)
+                        // picker.cols[4].values = []
+                        // picker.cols[4].values = days;
+                        // picker.cols[4].displayValues = daysNew;
+
+
+                        picker.setValue(values, 0);
+                        // picker.cols[4].value = currentValue
+                        // picker.cols[4].displayValue = displayValues[2]
+                        picker.updateValue();
+                        return false; // 因为数据未更新完，所以这里不进行后序的值的处理
+                    }
+
+                    //check min and max
+                    var current = new Date(values[0] + '-' + values[1] + '-' + values[2]);
+                    var valid = true;
+                    if (params.min) {
+                        var min = new Date(typeof params.min === "function" ? params.min() : params.min);
+
+                        if (current < +min) {
+                            picker.setValue(lastValidValues);
+                            valid = false;
+                        }
+                    }
+                    if (params.max) {
+                        var max = new Date(typeof params.max === "function" ? params.max() : params.max);
+                        if (current > +max) {
+                            picker.setValue(lastValidValues);
+                            valid = false;
+                        }
+                    }
+
+                    valid && (lastValidValues = values);
+
+                    if (self.params.onChange) {
+                        self.params.onChange.apply(this, arguments);
+                    }
+                },
+
+                formatValue: function (p, values, displayValues) {
+                    return self.params.format(p, values, displayValues);
+                },
+
+                cols: [
+                    {
+                        values: this.initYears
+                    },
+                    {
+                        divider: true,  // 这是一个分隔符
+                        content: params.yearSplit
+                    },
+                    {
+                        values: this.initMonthes
+                    },
+                    {
+                        divider: true,  // 这是一个分隔符
+                        content: params.monthSplit
+                    },
+                    {
+                        values: (function () {
+                            var dates = [];
+
+                            for (var i = 1; i <= 31; i++) {
+                                var dayNew = new Date(today.getFullYear(), today.getMonth(), i).getDay()
+                                switch (dayNew) {
+                                    case 0:
+                                        dayNew = '日'
+                                        break;
+                                    case 1:
+                                        dayNew = '一'
+                                        break;
+                                    case 2:
+                                        dayNew = '二'
+                                        break;
+                                    case 3:
+                                        dayNew = '三'
+                                        break;
+                                    case 4:
+                                        dayNew = '四'
+                                        break;
+                                    case 5:
+                                        dayNew = '五'
+                                        break;
+                                    case 6:
+                                        dayNew = '六'
+                                        break;
+                                }
+                                dayNew = '星期' + dayNew;
+                                dates.push(formatNumber(i) + dayNew);
+                                // dates.push(formatNumber(i));
+                            }
+                            return dates;
+                        })()
+                    },
+
+                ]
+            }
+
+            if (params.dateSplit) {
+                config.cols.push({
+                    divider: true,
+                    content: params.dateSplit
+                })
+            }
+
+            config.cols.push({
+                divider: true,
+                content: params.datetimeSplit
+            })
+
+            var times = self.params.times();
+            if (times && times.length) {
+                config.cols = config.cols.concat(times);
+            }
+
+            var inputValue = this.input.val();
+            if (inputValue) config.value = params.parse(inputValue);
+            if (this.params.value) {
+                this.input.val(this.params.value);
+                config.value = params.parse(this.params.value);
+            }
+
+            return config;
+        }
+    }
+
+    $.fn.datetimePicker = function (params) {
+        params = $.extend({}, defaults, params);
+        return this.each(function () {
+            if (!this) return;
+            var $this = $(this);
+            var datetime = $this.data("datetime");
+            if (!datetime) $this.data("datetime", new Datetime(this, params));
+            return datetime;
+        });
+    };
+
+    defaults = $.fn.datetimePicker.prototype.defaults = {
+        input: undefined, // 默认值
+        min: undefined, // YYYY-MM-DD 最大最小值只比较年月日，不比较时分秒
+        max: undefined,  // YYYY-MM-DD
+        yearSplit: '-',
+        monthSplit: '-',
+        dateSplit: '',  // 默认为空
+        datetimeSplit: ' ',  // 日期和时间之间的分隔符，不可为空
+        monthes: ('01 02 03 04 05 06 07 08 09 10 11 12').split(' '),
+        years: (function () {
+            var arr = [];
+            for (var i = 1950; i <= 2030; i++) {
+                arr.push(i);
+            }
+            return arr;
+        })(),
+        times: function () {
+            return [  // 自定义的时间
+                {
+                    values: (function () {
+                        var hours = [];
+                        for (var i = 0; i < 24; i++) hours.push(formatNumber(i));
+                        return hours;
+                    })()
+                },
+                {
+                    divider: true,  // 这是一个分隔符
+                    content: ':'
+                },
+                {
+                    values: (function () {
+                        var minutes = [];
+                        for (var i = 0; i < 60; i++) minutes.push(formatNumber(i));
+                        return minutes;
+                    })()
+                }
+            ];
+        },
+        format: function (p, values) { // 数组转换成字符串
+            return p.cols.map(function (col) {
+                return col.value || col.content;
+            }).join('');
+        },
+        parse: function (str) {
+            // 把字符串转换成数组，用来解析初始值
+            // 如果你的定制的初始值格式无法被这个默认函数解析，请自定义这个函数。比如你的时间是 '子时' 那么默认情况这个'时'会被当做分隔符而导致错误，所以你需要自己定义parse函数
+            // 默认兼容的分隔符
+            var t = str.split(this.datetimeSplit);
+            return t[0].split(/\D/).concat(t[1].split(/:|时|分|秒/)).filter(function (d) {
+                return !!d;
+            })
+        }
+    }
+
+}($);
